14d057c16861107e7e49883637a481a28a4e1abd
Cat Wong <catwong.nyc@gmail.com>
2016-09-10 20:57:06 -0400

    final submit

--- src/nyc/c4q/ac21/signboard/Main.java
+++ src/nyc/c4q/ac21/signboard/Main.java
@@ -45,29 +45,29 @@ public class Main {
      * @param text
      *   The text to scroll.
      */
-    public static void scrollTextScene(SignBoard board, String text) {
-        int width = board.getWidth();
-        int y = board.getHeight() / 2;
-        for (int x = -text.length(); x <= width; ++x) {
-            SignBoard.Frame frame = board.newFrame();
-
-            if (x >= width)
-                break;
-
-            if (x < 0)
-                // Scrolling on to the left side.
-                frame.write(0, y, text.substring(-x));
-
-            else if (x + text.length() <= width)
-                // Fully on the board.
-                frame.write(x, y, text);
-
-            else
-                // Scrolling off the board.
-                frame.write(x, y, text.substring(0, width - x));
-            frame.finish(0.05);
-        }
-    }
+//    public static void scrollTextScene(SignBoard board, String text) {
+//        int width = board.getWidth();
+//        int y = board.getHeight() / 2;
+//        for (int x = -text.length(); x <= width; ++x) {
+//            SignBoard.Frame frame = board.newFrame();
+//
+//            if (x >= width)
+//                break;
+//
+//            if (x < 0)
+//                // Scrolling on to the left side.
+//                frame.write(0, y, text.substring(-x));
+//
+//            else if (x + text.length() <= width)
+//                // Fully on the board.
+//                frame.write(x, y, text);
+//
+//            else
+//                // Scrolling off the board.
+//                frame.write(x, y, text.substring(0, width - x));
+//            frame.finish(0.05);
+//        }
+//    }
 
     /**
      * Draws a scene which flashes the words "POKEMON".
